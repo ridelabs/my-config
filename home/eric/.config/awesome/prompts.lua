4b1f44ccf9db77d8ef1de1d4fcd89b8ea83768d6
Eric Harrison <eharrison@vivint.com>
2017-01-10 10:46:28 -0700

    add in rename tab fn; fix wallpaper so we can use link to repo

--- home/eric/.config/awesome/prompts.lua
+++ home/eric/.config/awesome/prompts.lua
@@ -1,4 +1,14 @@
 globalkeys = awful.util.table.join(globalkeys,
+
+    --awful.key({ modkey, "Shift",  }, "F2",    function ()
+    awful.key({ modkey,  }, "F2",    function ()
+                    awful.prompt.run({ prompt = "Rename tab: ", text = awful.tag.selected().name, },
+                    mypromptbox[mouse.screen].widget,
+                    function (s)
+                        awful.tag.selected().name = s
+                    end)
+            end),
+
     -- Prompt
     awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
 
--- home/eric/bin/wallpaper.sh
+++ home/eric/bin/wallpaper.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-source=~/Pictures/Wallpaper
+source=~/Pictures/Wallpaper/
 export bg=$(find "$source" -maxdepth 1 -mindepth 1 -type f -print0 \
   | sort --zero-terminated --random-sort \
         | sed 's/\d000.*//g')
+echo "Setting to $bg"
 feh --no-fehbg --bg-max "$bg"
 
 
